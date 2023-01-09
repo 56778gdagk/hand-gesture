0b6905096465cb923d0ec96cc42e308d5e34c110
56778gdagk <91785115+56778gdagk@users.noreply.github.com>
2023-01-09 23:20:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+
+}
+
+function draw()
+{
+    
+}
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format :'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function captureImage()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/q2HiV0OeX/', modelLoaded);
