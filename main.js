56b884300c05dce2d37d9210cc85d41d14bfc7eb
56778gdagk <91785115+56778gdagk@users.noreply.github.com>
2023-01-12 22:16:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,3 +34,29 @@ function captureImage()
 console.log('ml5 version', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/q2HiV0OeX/', modelLoaded);
+
+function check()
+{
+    image = document.getElementById("image_captured");
+    classifier.classify(image,gotResults);
+}
+
+function gotResults(results,error)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+    }
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    var speak_data = "The prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
